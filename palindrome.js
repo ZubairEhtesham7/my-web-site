721fb8b42322e586535805c9359105327014fddc
ZubairEhtesham7 <zubair.ronaldo7@gmail.com>
2022-12-25 16:45:04 +0500

    html

--- palindrome.js
+++ palindrome.js
@@ -1,7 +1,21 @@
-import React from "react";
+function checkPalindrome(string) {
+  // find the length of a string
+  const len = string.length;
 
-function palindrome() {
-  return <div>palindrome</div>;
+  // loop through half of the string
+  for (let i = 0; i < len / 2; i++) {
+    // check if first and last string are same
+    if (string[i] !== string[len - 1 - i]) {
+      return "It is not a palindrome";
+    }
+  }
+  return "It is a palindrome";
 }
 
-export default palindrome;
+// take input
+const string = prompt("Enter a string: ");
+
+// call the function
+const value = checkPalindrome(string);
+
+console.log(value);
